c1379017dace169b625c5e03383b4fba984bcff9
Ahmad Nourallah <ahmadnurallah@gmail.com>
2024-12-05 08:41:56 -0500

    Create a separate module for home page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import "./styles.css";
+import Home from "./home";
+
+const Display = (function () {
+    const contentContainer = document.getElementById("content");
+    const homeBtn = document.querySelector(".home-btn");
+    const menuBtn = document.querySelector(".menu-btn");
+    const aboutBtn = document.querySelector(".about-btn");
+
+    homeBtn.addEventListener("click", event => {
+        event.preventDefault();
+        displayHome();
+    });
+
+    function emptyContentContainer() {
+        contentContainer.replaceChildren();
+    }
+
+    function displayHome() {
+        emptyContentContainer();
+        contentContainer.appendChild(Home.render(() => console.log("ok")));
+    }
+    
+    return { displayHome };
+
+})();
+
+Display.displayHome();
